f43d6e5e104cbbbc6ed55c1e187434bad6c2fdfb
Akriti <upadhayaakriti23@gmail.com>
2024-06-11 16:49:18 +0545

    globals

---
+++ day-1/day1.js
@@ -0,0 +1,14 @@
+// const amount = 1200
+// if(amount<1200){
+//     console.log("small number")
+// }
+// else{
+//     console.log("large number")
+// }
+// console.log("hey its my first node app")
+// console.log(__dirname)
+const names=require("./name")
+const sayhi=require("./app")
+sayhi('akriti')
+sayhi(names.john)
+sayhi(names.peter)
