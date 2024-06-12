1728a54ef02002f85cb2bfda51364b5f5383afab
Akriti <upadhayaakriti23@gmail.com>
2024-06-12 21:04:37 +0545

    http module

---
+++ day-3/day3.3.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('hello everyone');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000...');
+});
