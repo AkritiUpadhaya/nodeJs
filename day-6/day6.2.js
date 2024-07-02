2e1d868d0eb160a7152c55e5d07a8d9d36d567b9
Akriti <upadhayaakriti23@gmail.com>
2024-07-02 17:38:57 +0545

    adding parameters

--- day-6/day6.2.js
+++ day-6/day6.2.js
@@ -1,9 +1,9 @@
 const EventEmitter= require('events')
 const customEmitter= new EventEmitter()
-customEmitter.on('response',()=>{
-    console.log('data received')
+customEmitter.on('response',(name, id)=>{
+    console.log(`data received user: ${name} ${id}`)
 })
 customEmitter.on('response',()=>{
     console.log('additional info')
 })
-customEmitter.emit('response')
+customEmitter.emit('response', 'akriti', 1)
