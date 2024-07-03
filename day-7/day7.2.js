cb99dc0b9c04c7d425731a93014e1e7fbad89e35
Akriti <upadhayaakriti23@gmail.com>
2024-07-03 23:51:29 +0545

    http

--- day-7/day7.2.js
+++ day-7/day7.2.js
@@ -1,5 +1,5 @@
 const {createReadStream}= require('fs')
-const stream= createReadStream('./day-2/content/bigtext.txt')
+const stream= createReadStream('./day-2/content/bigtext.txt',{highWaterMark:1000})
 stream.on('data',(result)=>{
 console.log(result)
 })
