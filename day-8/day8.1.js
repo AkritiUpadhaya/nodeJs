0c778bb561136cc190d276454c1b5d8c81d77e8a
Akriti <upadhayaakriti23@gmail.com>
2024-07-08 19:49:33 +0545

    request objects

--- day-8/day8.1.js
+++ day-8/day8.1.js
@@ -1,8 +1,25 @@
 const http= require('http')
 const server= http.createServer((req,res)=>{
-    res.writeHead(200, {'content-type':'text/html'})
-    console.log('user hit the server')
-    res.write('<h>home page</>')
+    // console.log(req.method)
+    const url= req.url
+    if(url=='/'){
+        res.writeHead(200, {'content-type':'text/html'})
+    // console.log('user hit the server')
+    res.write('<h1>home page</h1>')
     res.end()
+    }
+    else if(url=='/about'){
+        res.writeHead(200, {'content-type':'text/html'})
+        // console.log('user hit the server')
+        res.write('<h1>About page</h1>')
+        res.end()
+    }
+ else{
+    res.writeHead(404, {'content-type':'text/html'})
+    // console.log('user hit the server')
+    res.write('<h1>Page not found</h1>')
+    res.end()
+ }
+   
 })
 server.listen(5000)
