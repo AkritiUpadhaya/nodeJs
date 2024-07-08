ea24f2187ca602f71c5f5bae3be386c14cefe160
Akriti <upadhayaakriti23@gmail.com>
2024-07-08 19:17:05 +0545

    http header

--- day-8/day8.1.js
+++ day-8/day8.1.js
@@ -1,5 +1,8 @@
 const http= require('http')
 const server= http.createServer((req,res)=>{
+    res.writeHead(200, {'content-type':'text/html'})
     console.log('user hit the server')
+    res.write('<h>home page</>')
+    res.end()
 })
 server.listen(5000)
