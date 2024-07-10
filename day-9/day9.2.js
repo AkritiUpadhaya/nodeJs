4ba42fd10ece37869a4ca79f7bce3730a16b749d
Akriti <upadhayaakriti23@gmail.com>
2024-07-10 18:50:33 +0545

    express-app simplified

--- day-9/day9.2.js
+++ day-9/day9.2.js
@@ -3,7 +3,7 @@ const path= require('path')
 const app= express()
 app.use(express.static('./public'))
 app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'../navbar-app/index.html'))
+    // res.sendFile(path.resolve(__dirname,'../navbar-app/index.html'))
 })
 app.all('*',(req, res)=>{
     res.status(404).send('resource not found')
