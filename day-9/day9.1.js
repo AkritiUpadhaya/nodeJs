7a61dfe7a062e1bef7e2db13659bb43d73f8e1f9
Akriti <upadhayaakriti23@gmail.com>
2024-07-10 13:37:28 +0545

    express basics

--- day-9/day9.1.js
+++ day-9/day9.1.js
@@ -1,5 +1,15 @@
 const express= require('express')
 const app= express()
-app.listen(3000, ()=>{
-    console.log('server is listening on port 3000')
+app.get('/',(req,res)=>{
+    res.status(200).send("home page")  
+})
+app.get('/about',(req,res)=>{
+    res.status(200).send("about page")  
+})
+app.get('*',(req,res)=>{
+    res.status(404).send("<h1>Page not found</h1>")  
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000')
 })
